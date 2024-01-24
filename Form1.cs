d848264d226a1a34a09e01c77aef89374254b952
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:20:11 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.AREA_OF_A_RECTANGLE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double A;
+            double H;
+            double L;
+            double P;
+
+            L = Convert.ToDouble(textBox1.Text);
+            H = Convert.ToDouble(textBox2.Text);
+         
+
+            A = L * H;
+            P = 2 * (L + H);
+            textBox3.Text = A.ToString();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double A;
+            double H;
+            double L;
+            double P;
+
+            L = Convert.ToDouble(textBox1.Text);
+            H = Convert.ToDouble(textBox2.Text);
+
+            A = L * H;
+            P = 2 * (L + H);
+
+            textBox4.Text = P.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.AREA_OF_A_RECTANGLE
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
